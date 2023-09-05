670a59922a50c483f78f986d44bd83604beababf
Tallia-Anne <erinawambiekele@gmail.com>
2023-09-05 02:08:44 +0200

    update

--- database/db.js
+++ database/db.js
@@ -3,18 +3,16 @@ require('dotenv').config();
 
 const db = {};
 
-const dbinfo = new Sequelize("social", "root", "", {
-    
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
+const dbinfo = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
     pool: {
         max: 5,
         min: 0,
     },
-    
-    
 });
+
 
 
 dbinfo
