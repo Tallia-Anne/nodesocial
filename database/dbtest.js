eda984b4c6aa740a8b31d500c96a2aec9910fac7
Tallia-Anne <erinawambiekele@gmail.com>
2023-09-09 23:49:52 +0200

    test

--- database/dbtest.js
+++ database/dbtest.js
@@ -1,17 +1,22 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
+
+console.log('\n les donnée:')
+console.log('Schema  :', process.env.database_URL)
+console.log('User    :', process.env.DB_USER)
+console.log('Passwd  :', process.env.DB_PASSWORD)
+console.log('Host    :', process.env.DB_HOST)
+console.log('Port    :', process.env.DB_PORT, '\n')
 
 const db = {};
 
-
-
-const dbinfo = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const dbinfo = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
-    pool: {
-        max: 5,
-        min: 0,
-    },
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 
@@ -22,7 +27,7 @@ dbinfo
         console.log("connexion de la base de données");
     })
     .catch((err) => {
-        console.error("il n'est pas connecter à la base de données: " + err);
+            console.error("il n'est pas connecter à la base de données: " + err);
     })
 
 
@@ -41,14 +46,14 @@ db.comments = require("../models/Comment")(dbinfo, Sequelize);
 db.users.hasMany(db.comments, { foreignKey: "userId" });
 db.postes.hasMany(db.image, { foreignKey: "postesId" });
 db.postes.hasMany(db.comments, { foreignKey: "postesId" });
+
 db.users.hasMany(db.postes, { foreignKey: "userId" });
 db.postes.belongsTo(db.users, { foreignKey: "userId" });
-db.comments.belongsTo(db.users, { foreignKey: "userId" });
 
 db.dbinfo = dbinfo;
 
 db.Sequelize = Sequelize;
 
-// dbinfo.sync({ force: true });
+ //dbinfo.sync({ force: true });
 
 module.exports = db;
